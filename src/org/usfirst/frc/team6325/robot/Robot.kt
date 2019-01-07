5aba32b7a291157289d2a3942800449df449b480
Cyco12 <PGD.daigle@gmail.com>
2019-01-07 02:20:43 -0500

    Updated gradle, Started on Drivetrain subystem and Robot.kt
    -Re-generated project to update to current (2019.1.1) WPILib project. Slight changes to build.gradle, settings.gradle and other gradle files to accommodate this.
    - Added Robot.kt. Still implementing some code in this (namely OI and Drivetrain right now)
    - Started Drivetrain.kt subsystem

--- src/org/usfirst/frc/team6325/robot/Robot.kt
+++ src/org/usfirst/frc/team6325/robot/Robot.kt
@@ -1,3 +1,82 @@
 // Rename to <robotname>.kt once we pick a robot name
 package org.usfirst.frc.team6325.robot
 
+import org.sertain.*
+import org.sertain.command.Command
+import edu.wpi.first.wpilibj.command.Scheduler
+
+// import commands
+import org.usfirst.frc.team6325.robot.subsystems.*
+
+
+class Robot : Robot()
+{
+    // Initialize subsystem instance objects for this script
+    public val drivetrain: Drivetrain = Drivetrain()
+
+    // auto command/chooser initilization goes here later?
+
+    // OI Initialization
+    //public var oi: OI = OI()    commented bc OI doesnt exist yet
+
+    // Runs on robot initialization; WPILib robotInit() equivalent
+    override fun onCreate()
+    {
+        // put any data to dashboard here
+    }
+
+    // Runs periodically when the robot is disabled; WPILib disabledPeriodic() equivalent
+    override fun executeDisabled()
+    {
+        Scheduler.getInstance().run()
+        // any dashboard data population here too
+    }
+
+    // Runs on autonomous(sandstorm) initialization; WPILib autonomousInit() equivalent
+    override fun onAutoStart()
+    {
+        /* auto code goes here later. for now, have a banana
+        
+         _
+        //\
+        V  \
+         \  \_
+          \,'.`-.
+           |\ `. `.
+           ( \  `. `-.                        _,.-:\
+            \ \   `.  `-._             __..--' ,-';/
+             \ `.   `-.   `-..___..---'   _.--' ,'/
+              `. `.    `-._        __..--'    ,' /
+                `. `-_     ``--..''       _.-' ,'
+                  `-_ `-.___        __,--'   ,'
+                     `-.__  `----"""    __.-'
+        hh                `--..____..--'
+         */
+    }
+
+    // Runs periodically during autonomous(sandstorm); WPILib autonomousPeriodic() equivalent
+    override fun executeAuto()
+    {
+        Scheduler.getInstance().run()
+        // put any dashboard data
+        // something to allow for interruption and transition to 'teleop' either at the end of the sandstorm or as soon as the driver takes control
+    }
+
+    // Runs on teleop initialization; WPILib teleopInit() equivalent
+    override fun onStart()
+    {
+        // zero navx yaw
+        // reset drivetrain encoders
+        // reset elevator encoders
+        // any other starting configurations
+        // nullcheck auto command and cancel it since telop is starting; this can eventually be replaced with smoother transition optimization to allow for a few seconds longer in auto control to allow for the sandstorm barrier to be fully up before drivers take control
+    }
+
+    // Runs periodically during teleop; WPILib teleopPeriodic() equivalent
+    override fun executeTeleop()
+    {
+        Scheduler.getInstance.run()
+        // put dashboard data here
+    }
+
+}
---
+++ src/org/usfirst/frc/team6325/robot/subsystems/Drivetrain.kt
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team6325.robot.subsytems
+
+import org.sertain.command.Subsystem
+import org.usfirst.frc.team6325.robot.Robot
+import org.usfirst.frc.team6325.robot.IDs
+//import org.usfirst.frc.team6325.robot.commands.Drive.ArcadeJoystickDrive
+import com.ctre.phoenix.motorcontrol.* // possibly deprecated
+import com.kanuailabs.navx.frc.AHRS
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Timer;
+
+
+class Drivetrain : Subsystem()
+{
+    
+}
