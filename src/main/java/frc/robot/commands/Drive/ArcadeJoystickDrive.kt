b771f1bc8d9e8ef6290e68ab29d9b2a54252e9b9
Unknown <PGD.daigle@gmail.com>
2019-01-10 21:18:10 -0500

    Updated some object declarations, fixed a lot of errors

--- src/main/java/frc/robot/commands/Drive/ArcadeJoystickDrive.kt
+++ src/main/java/frc/robot/commands/Drive/ArcadeJoystickDrive.kt
@@ -3,34 +3,37 @@ package frc.robot.commands.Drive
 import org.sertain.command.Command
 import frc.robot.Mag
 import frc.robot.subsystems.Drivetrain
+import frc.robot.OI
 
 
 public class ArcadeJoystickDrive : Command()
 {
     init
     {
-        requires(Drivetrain)
+        requires(Drivetrain())
     }
 
     override fun execute(): Boolean
     {
         // implement throttle slider
                 
-        var yDirection: Double = -Mag.oi.joystickLeft.getY()
-        var xDirection: Double = Mag.oi.joystickLeft.getX()
-        var spin: Double = Mag.oi.joystickLeft.getTwist() * .75
+        var yDirection: Double = -OI().joystickLeft.getY()
+        var xDirection: Double = -OI().joystickLeft.getX()
+        var spin: Double = OI().joystickLeft.getTwist() * .75
         var throttle: Double = 1.0 // replace with slider throttle later
 
-        if (Math.abs(yDirection) < Mag.drivetrain.deadzone) 
-            yDirection = 0
-        if (Math.abs(xDirection) < Mag.drivetrain.deadzone) 
-            xDirection = 0
-        if (Math.abs(spin) < Mag.drivetrain.deadzone)
-            spin = 0
+        if (Math.abs(yDirection) < Drivetrain().deadzone) 
+            yDirection = 0.0
+        if (Math.abs(xDirection) < Drivetrain().deadzone) 
+            xDirection = 0.0
+        if (Math.abs(spin) < Drivetrain().deadzone)
+            spin = 0.0
 
-        Mag.drivetrain.cartesianDrive(xDirection, yDirection, spin, throttle)
+        Drivetrain().cartesianDrive(xDirection, yDirection, spin, throttle)
+
+        return false;
     }
 
-    override fun isCanceled() = Mag.drivetrain.killMotors()
-    override fun onDestroy() = Mag.drivetrain.killMotors()
+    override fun isCanceled() = Drivetrain().killMotors()
+    override fun onDestroy() = Drivetrain().killMotors()
 }
