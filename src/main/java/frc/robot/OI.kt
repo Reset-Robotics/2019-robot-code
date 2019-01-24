cb61ddacca77ee00e55a827d16acb42413f87621
Unknown <PGD.daigle@gmail.com>
2019-01-24 18:20:51 -0500

    Cleaned up OI a bit
    - Removed a few unnecessary imports
    - Removed old commented code that is no-longer needed
    - Changed controller/joystick assingment to be by lazy
    - Moved around/clarified some comments
    - Changed up if statements to be more line efficient/visually appealing.

--- src/main/java/frc/robot/OI.kt
+++ src/main/java/frc/robot/OI.kt
@@ -12,25 +12,21 @@ import edu.wpi.first.wpilibj.GenericHID
 // Robot Imports
 import frc.robot.IDs
 import frc.robot.commands.Drive.*
-import frc.robot.commands.RBrake.Deploy
-import frc.robot.commands.RBrake.RBrakeSlave
-import frc.robot.commands.Drive.ToggleFieldOriented 
+import frc.robot.commands.RBrake.*
 import frc.robot.commands.Forklift.*
 import frc.robot.commands.BallIntake.*
 import frc.robot.subsystems.BallIntake
 import frc.robot.subsystems.RBrake
-//import frc.robot.subsystems.RBrake
 
 public class OI 
 {
     val ids: IDs = IDs()
     // Joysticks/Controllers
-    val joystickLeft: Joystick = Joystick((ids.joystickLeftIDs.get("USB-ID")) ?: 0)
-	//public val joystickRight: Joystick = Joystick((ids.joystickRightIDs.get("USB-ID")) ?: 0)
+    val joystickLeft by lazy { Joystick((ids.joystickLeftIDs.get("USB-ID")) ?: 0) }
 	val joystickRight by lazy { Joystick((ids.joystickRightIDs.get("USB-ID")) ?: 1) }
-    val xboxController: XboxController = XboxController((ids.xboxIDs.get("USB-ID")) ?: 2)
-	val xboxJoystickLeft: Joystick = Joystick((ids.xboxIDs.get("Left-Joystick-Y-Axis")) ?: 1)
-	val xboxJoystickRight: Joystick = Joystick((ids.xboxIDs.get("Right-Joystick-Y-Axis")) ?: 5)
+    val xboxController by lazy { XboxController((ids.xboxIDs.get("USB-ID")) ?: 2) }
+	val xboxJoystickLeft by lazy { Joystick((ids.xboxIDs.get("Left-Joystick-Y-Axis")) ?: 1) }
+	val xboxJoystickRight by lazy { Joystick((ids.xboxIDs.get("Right-Joystick-Y-Axis")) ?: 5) }
     
     //setting default trigger variable values
     var leftTrigger: Double = 0.0
@@ -40,31 +36,23 @@ public class OI
 
    fun OI() 
    {
-        //toggle field oriented
-        joystickRight.whenActive(ids.joystickRightIDs.get("Trigger") ?: 1, ToggleFieldOriented())
-
-        //toggle R-Brake
-        joystickLeft.whenActive(ids.joystickLeftIDs.get("Trigger") ?: 1, Deploy())
-
-        //Toggling Forklift Posistion
-        joystickLeft.whenActive(ids.joystickLeftIDs.get("Side-Thumb") ?:1, ToggleForklift())
+        joystickRight.whenActive(ids.joystickRightIDs.get("Trigger") ?: 1, ToggleFieldOriented()) // Toggle whether the drivetrain is field oriented or normal
+        joystickLeft.whenActive(ids.joystickLeftIDs.get("Trigger") ?: 1, Deploy()) // deploys the R-Brake in/out
+        
+        // TODO: Change to require being held down for a few seconds before triggering
+        joystickLeft.whenActive(ids.joystickLeftIDs.get("Side-Thumb") ?:1, ToggleForklift()) // deploys the forklift
 
         //Ball intake Controls
         leftTrigger = xboxController.getTriggerAxis(GenericHID.Hand.kLeft)
         rightTrigger = xboxController.getTriggerAxis(GenericHID.Hand.kRight)
 
-        if (Math.abs(xboxController.getTriggerAxis(GenericHID.Hand.kLeft)) < BallIntake.deadzone)
-            leftTrigger = 0.0
-        if (Math.abs(xboxController.getTriggerAxis(GenericHID.Hand.kRight)) < BallIntake.deadzone)
-            rightTrigger = 0.0
+        if (Math.abs(xboxController.getTriggerAxis(GenericHID.Hand.kLeft)) < BallIntake.deadzone) leftTrigger = 0.0
+        if (Math.abs(xboxController.getTriggerAxis(GenericHID.Hand.kRight)) < BallIntake.deadzone) rightTrigger = 0.0
         SpinIntake(leftTrigger, rightTrigger)
         
-        if(xboxController.getAButtonPressed())
-            ToggleAutoStop()
+        if(xboxController.getAButtonPressed()) ToggleAutoStop() // checks for auto interupt.
         
-        //RBrake slavery
+        // TODO: Change this to being just a default command run by the RBrake subsystem
         RBrakeSlave()
-            
-
    }
 }
