0a3b46b2b1150b18c4fd9f1915c4a07fe6669c7e
Unknown <hoxiealling@gmail.com>
2019-01-24 12:57:02 -0500

    Did more cleaning of the code

--- src/main/java/frc/robot/subsystems/Elevator.kt
+++ src/main/java/frc/robot/subsystems/Elevator.kt
@@ -140,7 +140,6 @@ public object Elevator : Subsystem()
     //elevator Motion Magic
     fun elevatorMM (newElevatorState: String = "Null")
     {
-        var targetPos = newElevatorState
         if(targetPos=="Bottom" && targetPos != elevatorState )
         {
             elevatorLeft.set(ControlMode.MotionMagic, bottomHeight)
