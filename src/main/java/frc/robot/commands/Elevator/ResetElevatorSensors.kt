b44d364cdd3647771b7cab625b9de59e1462c6e3
Unknown <PGD.daigle@gmail.com>
2019-01-31 19:31:36 -0500

    Fixed erroneous usage of Forklift subsystem/methods in Elevator command
    - Fixed usage of Forklift methods/subsystem in ResetElevatorSensors.kt Elevator command.

--- src/main/java/frc/robot/commands/Elevator/ResetElevatorSensors.kt
+++ src/main/java/frc/robot/commands/Elevator/ResetElevatorSensors.kt
@@ -1,14 +1,14 @@
-package frc.robot.commands.Forklift
+package frc.robot.commands.Elevator
 
 import org.sertain.command.Command
-import frc.robot.subsystems.Forklift
+import frc.robot.subsystems.Elevator
 
 public class ResetElevatorSensor: Command()
 {
     // resets all the sensors to do with the Forklist subsystem
     override fun execute(): Boolean
     {
-        Forklift.ResetEnconder() //reseting both motor encoders on the forklift to have the same "zeroed" state
+        Elevator.ResetEncoder() //reseting both motor encoders on the forklift to have the same "zeroed" state
         //will reset gyro when gyro code is installed
         return true;
     }
