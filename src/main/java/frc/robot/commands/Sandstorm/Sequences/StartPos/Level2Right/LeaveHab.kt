90c5fd3ea330299b45af997d5efb30891dcc4e11
Unknown <PGD.daigle@gmail.com>
2019-02-25 17:46:10 -0500

    Added paths for just leaving hab platforms level 1 and 2
    Added default paths for each starting position for leaving the hab.

---
+++ src/main/java/frc/robot/commands/Sandstorm/Sequences/StartPos/Level2Right/LeaveHab.kt
@@ -0,0 +1,16 @@
+package frc.robot.commands.Sandstorm.Sequences.StartPos.Level2Right
+
+import org.sertain.command.Command
+import frc.robot.commands.Drive.Auto.DriveByTime
+import frc.robot.commands.RBrake.Deploy
+
+public class LeaveHab : Command()
+{
+    override fun execute(): Boolean
+    {
+        Deploy()
+        DriveByTime(0.0, -1.0, 0.0, 1.0, 2.0) //xdir, ydir, angle, throttle, time
+        
+        return true;
+    }
+}
