d0edd14b55b97c6731cbc9a5f41049ae5829b792
Unknown <PGD.daigle@gmail.com>
2019-02-25 17:33:04 -0500

    Added shell auto class structure for Level 1 and 2 starts to both rockets
    - Added all rocket left and rocket right classes for level 1 right side start
    - Added all rocket left and rocket right classes for level 1 middle side start
    - Added all rocket left and rocket right classes for level 1 left side start
    - Added all rocket left and rocket right classes for level 2 right side start
    - Added all rocket left and rocket right classes for level 2 left side start

---
+++ src/main/java/frc/robot/commands/Sandstorm/Sequences/StartPos/Level1Right/Rocket-Left/Level1Right/2Panel.kt
@@ -0,0 +1,14 @@
+package frc.robot.commands.Sandstorm.Sequences.StartPos.Level1Right.RocketLeft.Level1Right
+
+import org.sertain.command.Command
+import frc.robot.commands.Drive.Auto.DriveByTime
+
+public class 2Panel : Command()
+{
+    override fun execute(): Boolean
+    {
+        DriveByTime(0.0, -1.0, 0.0, 0.4, 2.0) //xdir, ydir, angle, throttle, time
+        
+        return true;
+    }
+}
---
+++ src/main/java/frc/robot/commands/Sandstorm/Sequences/StartPos/Level2Left/RocketRight/Level3Right/1Panel1Cargo.kt
@@ -0,0 +1,14 @@
+package frc.robot.commands.Sandstorm.Sequences.StartPos.Level2Left.RocketRight.Level3Right
+
+import org.sertain.command.Command
+import frc.robot.commands.Drive.Auto.DriveByTime
+
+public class 1Panel1Cargo : Command()
+{
+    override fun execute(): Boolean
+    {
+        DriveByTime(0.0, -1.0, 0.0, 0.4, 2.0) //xdir, ydir, angle, throttle, time
+        
+        return true;
+    }
+}
